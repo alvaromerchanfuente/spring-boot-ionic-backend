e93b06e4f27b10eeef2a15760ad13e1bef74fcab
alvaromerchanfuente <alvaromerchanf@gmail.com>
2019-09-17 14:51:55 -0300

    Criando operacao de instanciacao

--- src/main/java/com/alvaromerchan/projectx/ProjectxApplication.java
+++ src/main/java/com/alvaromerchan/projectx/ProjectxApplication.java
@@ -1,13 +1,32 @@
 package com.alvaromerchan.projectx;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjectxApplication {
+import com.alvaromerchan.projectx.domain.Categoria;
+import com.alvaromerchan.projectx.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjectxApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
