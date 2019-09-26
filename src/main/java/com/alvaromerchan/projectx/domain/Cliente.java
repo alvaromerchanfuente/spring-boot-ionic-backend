7c257c1dd91d242ec6888cf4fcb76f9a258e0a49
alvaromerchanfuente <alvaromerchanf@gmail.com>
2019-09-26 11:39:07 -0300

    Pedido, EstadoPagamento e Pagamento - Parte 1

--- src/main/java/com/alvaromerchan/projectx/domain/Cliente.java
+++ src/main/java/com/alvaromerchan/projectx/domain/Cliente.java
@@ -35,6 +35,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -103,6 +105,15 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -127,6 +138,6 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
+
 }
