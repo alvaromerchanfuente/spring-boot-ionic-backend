c8b5d0b2cf5ceb3a33a1145a14d23049286499f1
alvaromerchanfuente <alvaromerchanf@gmail.com>
2019-10-21 11:28:40 -0300

    MODIFICAÇÃO no listar todas categorias com DTO

--- src/main/java/com/alvaromerchan/projectx/resources/CategoriaResource.java
+++ src/main/java/com/alvaromerchan/projectx/resources/CategoriaResource.java
@@ -1,10 +1,13 @@
 package com.alvaromerchan.projectx.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,10 +51,13 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		Categoria obj = service.find(id);
-		return ResponseEntity.ok().body(obj);
+	
+	@Autowired
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list= service.findAll();
+		List <CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 	
